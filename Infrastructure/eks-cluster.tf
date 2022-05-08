52859976426eb7c1db44db745caea7644a8a4611
ShanMarshBubashan <bubashanb1@gmail.com>
2022-05-08 22:27:31 +0100

    Actions edited

--- Infrastructure/eks-cluster.tf
+++ Infrastructure/eks-cluster.tf
@@ -16,13 +16,13 @@ module "eks" {
       name                          = "worker-group-1"
       instance_type                 = "t2.small"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-      asg_desired_capacity          = 2
+      asg_desired_capacity          = 1
     },
     {
       name                          = "worker-group-2"
       instance_type                 = "t2.small"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 2
+      asg_desired_capacity          = 1
     },
   ]
 }
--- Infrastructure/outputs.tf
+++ Infrastructure/outputs.tf
@@ -32,3 +32,5 @@ output "cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = local.cluster_name
 }
+
+
